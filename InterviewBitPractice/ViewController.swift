6ff6466b49356f423671ace8e17c91a6a113fc67
Chivonne Reji <chivonne.reji@olx.com>
2021-04-06 19:07:05 +0530

    Initial commit with project files

---
+++ InterviewBitPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterviewBitPractice
+//
+//  Created by Chivonne Reji on 06/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
